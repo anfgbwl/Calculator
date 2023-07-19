52f66ff1fb3b6c66a9181ddc38d011c46ed91c43
Seoon KIM <6872065@gmail.com>
2023-07-19 16:14:43 +0900

    Calculator class abstract

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -31,10 +31,10 @@ class Calculator {
      
      */
     
-    private let addOperation = AddOperation()
-    private let substractOperation = SubstractOperation()
-    private let multiplyOperation = MultiplyOperation()
-    private let divideOperation = DivideOperation()
+    private let addOperation: AbstractOperation = AddOperation()
+    private let substractOperation: AbstractOperation = SubstractOperation()
+    private let multiplyOperation: AbstractOperation = MultiplyOperation()
+    private let divideOperation: AbstractOperation = DivideOperation()
         
     func calculate(_ num1: Double, _ num2: Double, operatorSymbol: String) -> Double {
         switch operatorSymbol {
@@ -56,27 +56,33 @@ class Calculator {
     
 }
 
-
-class AddOperation {
+class AbstractOperation {
     func calculate(_ num1: Double, _ num2: Double) -> Double {
+        fatalError("ERROR: Need to modify Operation class")
+    }
+    
+}
+
+class AddOperation: AbstractOperation {
+    override func calculate(_ num1: Double, _ num2: Double) -> Double {
         return num1 + num2
     }
 }
 
-class SubstractOperation {
-    func calculate(_ num1: Double, _ num2: Double) -> Double {
+class SubstractOperation: AbstractOperation {
+    override func calculate(_ num1: Double, _ num2: Double) -> Double {
         return num1 - num2
     }
 }
 
-class MultiplyOperation {
-    func calculate(_ num1: Double, _ num2: Double) -> Double {
+class MultiplyOperation: AbstractOperation {
+    override func calculate(_ num1: Double, _ num2: Double) -> Double {
         return num1 * num2
     }
 }
 
-class DivideOperation {
-    func calculate(_ num1: Double, _ num2: Double) -> Double {
+class DivideOperation: AbstractOperation {
+    override func calculate(_ num1: Double, _ num2: Double) -> Double {
         guard num2 != 0 else {
             print("ERROR: Division by zero")
             return 0
